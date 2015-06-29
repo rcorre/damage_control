c1b68d96c344da542a9a71d20b65e66c85435f26
Ryan <ryan@rcorre.net>
2015-06-29 09:04:50 -0400

    Show starting wall regions on mouse hover.

--- src/states/battle.d
+++ src/states/battle.d
@@ -7,7 +7,7 @@ import dau;
 import dtiled;
 import tilemap;
 import player;
-import states.place_wall;
+import states.choose_base;
 
 private enum {
   cannonSpriteRow = 6,
@@ -40,7 +40,7 @@ class Battle : State!Game {
       this.states = new StateStack!Battle(this);
       _tileAtlas = game.content.bitmaps.get("tileset");
       player = new Player(Color(0, 0, 0.8));
-      states.push(new PlaceWall);
+      states.push(new ChooseBase);
     }
 
     void run(Game game) {
@@ -73,7 +73,17 @@ class Battle : State!Game {
 
 private:
 struct BattleData {
-  private Vector2f[][] _enemyWaves;
+  struct WallRegion {
+    RowCol start, end;
+
+    this(ObjectData obj, int tileWidth, int tileHeight) {
+      this.start = RowCol(obj.y / tileHeight, obj.x / tileWidth);
+      this.end = RowCol((obj.y + obj.height) / tileHeight, (obj.x + obj.width) / tileWidth);
+    }
+  }
+
+  private Vector2f[][] _enemyWaves;  // _enemyWaves[i] lists the enemy positions in wave #i
+  private WallRegion[] _wallRegions; // walls that surround starting positions
 
   this(MapData data) {
     auto parseWave(LayerData layer) {
@@ -87,9 +97,35 @@ struct BattleData {
       .filter!(x => x.name.startsWith("enemies"))
       .map!(wave => parseWave(wave))
       .array;
+
+    _wallRegions = data.getLayer("walls")
+      .objects
+      .map!(obj => WallRegion(obj, data.tileWidth, data.tileHeight))
+      .array;
   }
 
   auto getEnemyWave(int round) {
     return _enemyWaves[round];
+  }
+
+  auto getWallCoordsForNode(RowCol coord) {
+    auto region = _wallRegions
+      .find!(region =>
+        region.start.col <= coord.col &&
+        region.start.row <= coord.row &&
+        region.end.col   >= coord.col &&
+        region.end.row   >= coord.row)
+      .front;
+
+    auto topLeft  = region.start;
+    auto topRight = RowCol(region.start.row, region.end.col);
+    auto botLeft  = RowCol(region.end.row  , region.start.col);
+    auto botRight = region.end;
+
+    return chain(
+        topLeft.span(topRight  + RowCol(1,0)),  // top row
+        botLeft.span(botRight  + RowCol(1,0)),  // bottom row
+        topLeft.span(botLeft   + RowCol(0,1)),  // left column
+        topRight.span(botRight + RowCol(1,1))); // right column
   }
 }
---
+++ src/states/choose_base.d
@@ -0,0 +1,43 @@
+module states.choose_base;
+
+import std.range;
+import std.array;
+import std.random;
+import std.algorithm;
+import dau;
+import dtiled;
+import jsonizer;
+import tilemap;
+import states.battle;
+import states.place_wall;
+
+/// Player is holding a wall segment they can place with a mouse click
+class ChooseBase : State!Battle {
+  private RowCol _currentCoord;
+
+  override {
+    void run(Battle battle) {
+      auto game = battle.game;
+      auto map = battle.map;
+
+      auto newCoord = map.coordAtPoint(game.input.mousePos);
+
+      if (newCoord != _currentCoord && map.tileAt(newCoord).hasNode) {
+        // clear old walls
+        if (map.tileAt(_currentCoord).hasNode) {
+          foreach(coord ; battle.data.getWallCoordsForNode(_currentCoord)) {
+            map.tileAt(coord).construct = Construct.none;
+          }
+        }
+
+        // place new walls
+        foreach(coord ; battle.data.getWallCoordsForNode(newCoord)) {
+          map.tileAt(coord).construct = Construct.wall;
+        }
+
+        // register the current coord as the selected node
+        _currentCoord = newCoord;
+      }
+    }
+  }
+}
