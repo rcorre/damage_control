89be1984f02e74882458972d4499f39255332803
Ryan <ryan@rcorre.net>
2015-07-16 13:01:25 -0400

    Show text in place_cannons state.
    Draw title, phase timer, and cannon count to top of screen.
    TODO:
    - Battle phase text
    - Rebuild phase text
    - Choose reactor text
    
    Possibly introduce a common base class for these states.

--- src/states/place_cannons.d
+++ src/states/place_cannons.d
@@ -1,6 +1,7 @@
 module states.place_cannons;
 
 import std.range     : walkLength;
+import std.format    : format;
 import std.algorithm : count, filter;
 import dau;
 import dtiled;
@@ -12,16 +13,30 @@ private enum {
   cannonDepth     = 3,
   cannonsPerRound = 1,
   cannonsPerNode  = 1,
-  tilesPerCannon  = 30
+  tilesPerCannon  = 30,
+
+  fontName  = "Mecha",
+  fontSize  = 24,
+  textDepth = 1,
+
+  titleText         = "Install Cannons",
+  cannonCountFormat = "Cannons: %d",
+  timerCountFormat  = "Time: %2.1f",
+
+  timerPos       = Vector2i(10, 10),
+  titlePos       = Vector2i(300, 10),
+  cannonCountPos = Vector2i(600, 10),
 }
 
 /// Player may place cannons within wall bounds
 class PlaceCannons : State!Battle {
-  private float  _timer;
-  private ulong  _cannons;
+  private float _timer;
+  private ulong _cannons;
+  private Font  _font;
 
   override {
     void enter(Battle battle) {
+      _font = battle.game.fonts.get(fontName, fontSize);
       _timer = phaseTime;
 
       auto territory = battle.map.allTiles.filter!(x => x.isEnclosed);
@@ -58,6 +73,34 @@ class PlaceCannons : State!Battle {
       // tick down the timer; if it hits 0 or we are done placing cannons, pop the state
       _timer -= game.deltaTime;
       if (_timer < 0 || _cannons == 0) battle.states.pop();
+
+      drawText(game.renderer);
     }
+  }
+
+  private void drawText(Renderer renderer) {
+    auto batch = TextBatch(_font, textDepth);
+
+    Text text;
+
+    // title
+    text.color     = Color.gray;
+    text.transform = titlePos;
+    text.text      = titleText;
+    batch ~= text;
+
+    // timer
+    text.color = (_timer > 3.0f) ? Color.gray : Color.red;
+    text.transform = timerPos;
+    text.text      = timerCountFormat.format(_timer);
+    batch ~= text;
+
+    // cannon counter
+    text.color = (_cannons > 0) ? Color.gray : Color.red;
+    text.transform = cannonCountPos;
+    text.text      = cannonCountFormat.format(_cannons);
+    batch ~= text;
+
+    renderer.draw(batch);
   }
 }
