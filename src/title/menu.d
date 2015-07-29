1fbb4439b1b3592e972d6024d24a27cb419b7562
Ryan <ryan@rcorre.net>
2015-07-28 22:06:21 -0400

    Move menu logic into separate object.

---
+++ src/title/menu.d
@@ -0,0 +1,137 @@
+/// Title menu
+module title.menu;
+
+import std.math      : pow;
+import std.container : Array;
+import battle.battle;
+import dau;
+
+private enum {
+  fontName  = "Mecha",
+  fontSize  = 36,
+  textDepth = 1,
+
+  menuCenter      = Vector2i(400, 200),
+  entryMargin     = Vector2i(  0, 100),
+  underlineOffset = Vector2i(  0,  30),
+  underlineSize   = Vector2i(150,   6),
+
+  selectedTint   = Color(1f,1f,1f,1f),
+  unselectedTint = Color(1f,1f,1f,0.3f),
+
+  transitionDuration = 0.5,
+}
+
+/// Show the title screen.
+class TitleMenu {
+  private {
+    Array!MenuEntry    _entries;
+    Array!EventHandler _handlers;
+    Font               _font;
+    uint               _selectedEntry;
+    Bitmap             _underlineBmp;
+  }
+
+  this(Game game, MenuEntry[] entries ...) {
+    _underlineBmp = Bitmap(al_create_bitmap(underlineSize.x, underlineSize.y));
+
+    al_set_target_bitmap(_underlineBmp);
+    al_clear_to_color(Color.white);
+    al_set_target_backbuffer(game.display.display);
+
+    _font = game.fonts.get(fontName, fontSize);
+
+    //auto entryPos(int idx) { return menuCenter + entryMargin * idx; }
+
+    _entries = entries;
+
+    _entries[0].selected = true;
+  }
+
+  void moveSelectionDown() {
+    if (_selectedEntry < _entries.length - 1) {
+      _entries[_selectedEntry].selected   = false;
+      _entries[++_selectedEntry].selected = true;
+    }
+  }
+
+  void moveSelectionUp() {
+      if (_selectedEntry > 0) {
+        _entries[_selectedEntry].selected   = false;
+        _entries[--_selectedEntry].selected = true;
+      }
+  }
+
+  void confirmSelection(Game game) {
+    assert(_selectedEntry >= 0 && _selectedEntry < _entries.length);
+    _entries[_selectedEntry].action(game);
+  }
+
+  void update(Game game) {
+    auto textBatch   = TextBatch(_font, textDepth);
+    auto spriteBatch = SpriteBatch(_underlineBmp, textDepth);
+
+    foreach(ref entry ; _entries) {
+      entry.update(game.deltaTime);
+
+      Text text;
+      Sprite sprite;
+
+      text.centered  = true;
+      text.color     = entry.tint;
+      text.transform = entry.center;
+      text.text      = entry.text;
+
+      sprite.centered  = true;
+      sprite.color     = entry.tint;
+      sprite.transform = entry.underlinePos;
+      sprite.region    = Rect2i(Vector2i.zero, underlineSize);
+
+      textBatch   ~= text;
+      spriteBatch ~= sprite;
+    }
+
+    game.renderer.draw(textBatch);
+    game.renderer.draw(spriteBatch);
+  }
+}
+
+package struct MenuEntry {
+  alias Action = void delegate(Game);
+
+  Vector2i center;
+  string   text;
+  bool     selected;
+  float    progress;
+  Action   action;
+
+  this(string text, Vector2i center, Action action) {
+    this.text     = text;
+    this.center   = center;
+    this.action   = action;
+    this.progress = 0;
+  }
+
+  void update(float time) {
+    if (selected && progress < transitionDuration) {
+      progress += time;
+    }
+    else if (!selected && progress > 0) {
+      progress -= time;
+    }
+  }
+
+  auto underlinePos() {
+    auto start = Vector2i(-100, center.y) + underlineOffset;
+    auto end   = center + underlineOffset;
+
+    // increases from 0 to 1, starting quickly then slowing down
+    auto factor = (progress / transitionDuration).pow(0.35);
+
+    return start.lerp(end, factor);
+  }
+
+  auto tint() {
+    return unselectedTint.lerp(selectedTint, progress / transitionDuration);
+  }
+}
--- src/title/title.d
+++ src/title/title.d
@@ -5,80 +5,37 @@ import std.math      : pow;
 import std.container : Array;
 import battle.battle;
 import dau;
-
-private enum {
-  fontName  = "Mecha",
-  fontSize  = 36,
-  textDepth = 1,
-
-  menuCenter      = Vector2i(400, 200),
-  entryMargin     = Vector2i(  0, 100),
-  underlineOffset = Vector2i(  0,  30),
-  underlineSize   = Vector2i(150,   6),
-
-  selectedTint   = Color(1f,1f,1f,1f),
-  unselectedTint = Color(1f,1f,1f,0.3f),
-
-  transitionDuration = 0.5,
-}
+import title.menu;
 
 /// Show the title screen.
 class Title : State!Game {
   private {
-    Array!MenuEntry    _entries;
-    Array!EventHandler _handlers;
-    Font               _font;
-    uint               _selectedEntry;
-    Bitmap             _underlineBmp;
+    TitleMenu _menu;
+    EventHandler _handler;
   }
 
   override {
     void enter(Game game) {
-      if (_underlineBmp is null) {
-        // TODO: what a hack ...
-        // problem here is that the renderer is holding on to the bitmap through
-        // the SpriteBatch
-        // eventually need to have the renderer manage bitmaps itself.
-
-        // create underline bitmap
-        _underlineBmp = al_create_bitmap(underlineSize.x, underlineSize.y);
-
-        al_set_target_bitmap(_underlineBmp);
-        al_clear_to_color(Color.white);
-        al_set_target_backbuffer(game.display.display);
-      }
-
-      _font = game.fonts.get(fontName, fontSize);
-
-      auto menuPos(int idx) { return menuCenter + entryMargin * idx; }
-
       auto play(Game game)    { game.states.push(new Battle); }
       auto options(Game game) { game.states.push(new Battle); }
       auto quit(Game game)    { game.stop(); }
 
-      _entries.insert(MenuEntry("Play"    , menuPos(0), &play));
-      _entries.insert(MenuEntry("Options" , menuPos(1), &options));
-      _entries.insert(MenuEntry("Quit"    , menuPos(2), &quit));
-
-      _entries[0].selected = true;
+      _menu = new TitleMenu(
+          game,
+          MenuEntry("Play"    , Vector2i(400, 200), &play),
+          MenuEntry("Options" , Vector2i(400, 300), &options),
+          MenuEntry("Quit"    , Vector2i(400, 400), &quit));
 
       void handleKeyDown(in ALLEGRO_EVENT ev) {
         switch (ev.keyboard.keycode) {
           case ALLEGRO_KEY_J:
-            assert(_selectedEntry >= 0 && _selectedEntry < _entries.length);
-            _entries[_selectedEntry].action(game);
+            _menu.confirmSelection(game);
             break;
           case ALLEGRO_KEY_W:
-            if (_selectedEntry > 0) {
-              _entries[_selectedEntry].selected   = false;
-              _entries[--_selectedEntry].selected = true;
-            }
+            _menu.moveSelectionUp();
             break;
           case ALLEGRO_KEY_S:
-            if (_selectedEntry < _entries.length - 1) {
-              _entries[_selectedEntry].selected   = false;
-              _entries[++_selectedEntry].selected = true;
-            }
+            _menu.moveSelectionDown();
             break;
           case ALLEGRO_KEY_ESCAPE:
             game.stop();
@@ -87,82 +44,15 @@ class Title : State!Game {
         }
       }
 
-      _handlers.insert(game.events.onKeyDown(&handleKeyDown));
+      _handler = game.events.onKeyDown(&handleKeyDown);
     }
 
     void exit(Game game) {
-      foreach(handler ; _handlers) handler.unregister();
-      _handlers.clear();
-
-      if (_underlineBmp !is null) al_destroy_bitmap(_underlineBmp);
+      _handler.unregister();
     }
 
     void run(Game game) {
-      auto textBatch   = TextBatch(_font, textDepth);
-      auto spriteBatch = SpriteBatch(_underlineBmp, textDepth);
-
-      foreach(ref entry ; _entries) {
-        entry.update(game.deltaTime);
-
-        Text text;
-        Sprite sprite;
-
-        text.centered  = true;
-        text.color     = entry.tint;
-        text.transform = entry.center;
-        text.text      = entry.text;
-
-        sprite.centered  = true;
-        sprite.color     = entry.tint;
-        sprite.transform = entry.underlinePos;
-        sprite.region    = Rect2i(Vector2i.zero, underlineSize);
-
-        textBatch   ~= text;
-        spriteBatch ~= sprite;
-      }
-
-      game.renderer.draw(textBatch);
-      game.renderer.draw(spriteBatch);
+      _menu.update(game);
     }
-  }
-}
-
-private struct MenuEntry {
-  alias Action = void delegate(Game);
-
-  Vector2i center;
-  string   text;
-  bool     selected;
-  float    progress;
-  Action   action;
-
-  this(string text, Vector2i center, Action action) {
-    this.text     = text;
-    this.center   = center;
-    this.action   = action;
-    this.progress = 0;
-  }
-
-  void update(float time) {
-    if (selected && progress < transitionDuration) {
-      progress += time;
-    }
-    else if (!selected && progress > 0) {
-      progress -= time;
-    }
-  }
-
-  auto underlinePos() {
-    auto start = Vector2i(-100, center.y) + underlineOffset;
-    auto end   = center + underlineOffset;
-
-    // increases from 0 to 1, starting quickly then slowing down
-    auto factor = (progress / transitionDuration).pow(0.35);
-
-    return start.lerp(end, factor);
-  }
-
-  auto tint() {
-    return unselectedTint.lerp(selectedTint, progress / transitionDuration);
   }
 }
