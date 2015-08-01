3e445ebf82e1117c11d3b7d5e30d4b0a2bd24947
Ryan <ryan@rcorre.net>
2015-08-01 14:00:42 -0400

    Fill out the title menu.

--- src/title/title.d
+++ src/title/title.d
@@ -60,28 +60,60 @@ class Title : State!Game {
   }
 
   private:
-  auto mainMenu(Game game) {
-    auto play(Game game)    {
-      _menus.back.centerToStack();
-      _menus ~= playMenu(game);
-      _menus.back.exitToCenter();
-    }
+  void pushMenu(TitleMenu menu) {
+    _menus.back.centerToStack();
+    _menus ~= menu;
+    _menus.back.exitToCenter();
+  }
 
-    auto options(Game game) { game.states.push(new Battle); }
-    auto quit(Game game)    { game.stop(); }
+  auto mainMenu(Game game) {
+    auto play(Game game)     { pushMenu(playMenu(game)); }
+    auto options(Game game)  { pushMenu(optionsMenu(game)); }
+    auto controls(Game game) { pushMenu(controlsMenu(game)); }
+    auto credits(Game game)  { pushMenu(creditsMenu(game)); }
+    auto quit(Game game)     { game.stop(); }
 
     return new TitleMenu(game,
-        MenuEntry("Play"   , Vector2i(300, 200), &play),
-        MenuEntry("Options", Vector2i(300, 300), &options),
-        MenuEntry("Quit"   , Vector2i(300, 400), &quit));
+        MenuEntry("Play"    , Vector2i(300, 100), &play),
+        MenuEntry("Options" , Vector2i(300, 200), &options),
+        MenuEntry("Controls", Vector2i(300, 300), &controls),
+        MenuEntry("Credits" , Vector2i(300, 400), &credits),
+        MenuEntry("Quit"    , Vector2i(300, 500), &quit));
   }
 
   auto playMenu(Game game) {
     auto play(Game game) { game.states.push(new Battle); }
 
     return new TitleMenu(game,
-        MenuEntry("Level1", Vector2i(300, 200), &play),
-        MenuEntry("Level2", Vector2i(300, 300), &play),
-        MenuEntry("Level3", Vector2i(300, 400), &play));
+        MenuEntry("Tutorial", Vector2i(400, 200), &play),
+        MenuEntry("1 Player", Vector2i(400, 300), &play),
+        MenuEntry("2 Player", Vector2i(400, 400), &play));
+  }
+
+  auto optionsMenu(Game game) {
+    auto dummy(Game game) {}
+
+    return new TitleMenu(game,
+        MenuEntry("Sound", Vector2i(400, 200), &dummy),
+        MenuEntry("Music", Vector2i(400, 400), &dummy));
+  }
+
+  auto controlsMenu(Game game) {
+    auto dummy(Game game) {}
+
+    return new TitleMenu(game,
+        MenuEntry("Sound", Vector2i(400, 200), &dummy),
+        MenuEntry("Music", Vector2i(400, 400), &dummy));
+  }
+
+  auto creditsMenu(Game game) {
+    auto dummy(Game game) {}
+
+    return new TitleMenu(game,
+        MenuEntry("D"       , Vector2i(400, 100), &dummy),
+        MenuEntry("Allegro" , Vector2i(400, 200), &dummy),
+        MenuEntry("Aseprite", Vector2i(400, 300), &dummy),
+        MenuEntry("LMMS"    , Vector2i(400, 400), &dummy),
+        MenuEntry("Tiled"   , Vector2i(400, 500), &dummy));
   }
 }
