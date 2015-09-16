415abec65046f0cd7d083834553fe67d73f3c8ec
Ryan Roden-Corrent <ryan@rcorre.net>
2015-09-16 12:52:43 -0400

    Add function drawKeyIcon.
    Draws a small white rect wrapped around a key name.
    Can be used to draw input hints:
    
    TODO:
    batching for drawing multiple keys.
    lookup key matching action so key shown matches user's keymap.

---
+++ src/common/key_icon.d
@@ -0,0 +1,36 @@
+module common.key_icon;
+
+import cid;
+
+private enum {
+  pixelBuffer = 8
+}
+
+void drawKeyIcon(string keyname, Vector2i center, Game game) {
+  auto font = game.fonts.get("Mecha", 16);
+
+  Text text;
+
+  text.text      = keyname;
+  text.color     = Color.white;
+  text.centered  = true;
+  text.transform = center;
+
+  RectPrimitive prim;
+
+  prim.rect = Rect2i.centeredAt(
+      center,
+      font.widthOf(keyname)  + pixelBuffer,
+      font.heightOf(keyname) + pixelBuffer);
+
+  prim.color = Color.white;
+
+  auto tb = TextBatch(font, 6);
+  auto pb = PrimitiveBatch(5);
+
+  pb ~= prim;
+  tb ~= text;
+
+  game.renderer.draw(pb);
+  game.renderer.draw(tb);
+}
